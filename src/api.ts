9b0bc945f6b93e5228e423b97dda1514d93e56e0
JPKingtero <JPQM4897@gmail.com>
2025-02-25 00:07:42 -0500

    test(api.ts): creating a test for the function createANewPet

--- src/api.ts
+++ src/api.ts
@@ -1,33 +1,101 @@
 import { PetRequests } from "./petRequest";
 
-const petAPI = new PetRequests();
-
-export async function createANewPet(){
-  //const petAPI = new PetRequests();
-  try{
-    const newPet = petAPI.addNewPet({
-      "id": 1017249723,
-      "name": "Cosmo",
-      "category": {
-        "id": 1,
-        "name": "Dogs"
-      },
-      "photoUrls": [
-        "string"
-      ],
-      "tags": [
-        {
-          "id": 8,
-          "name": "friendly"
-        }
-      ],
-      "status": "available"
-  });
-  const data = await petAPI.makeARequest(newPet)
-  console.log("Pet added successfully:", data)
-}catch(error){
-  console.error ( "Error adding pet:", error)
+interface APIRequest {
+  method: "GET" | "POST" | "PUT" | "DELETE" ;
+  body?: any;
+  headers: Record<string,string>;
 }
+
+interface pet_info {
+  "id": number,
+  "name": string,
+  "category": {
+    "id": number,
+    "name": string
+  },
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": number,
+      "name": string
+    }
+  ],
+  "status": string
+}
+
+const petAPI = new PetRequests();
+const baseURL = "https://petstore3.swagger.io/api/v3";
+const cosmo: pet_info= {
+    "id": 1017249723,
+    "name": "Cosmo",
+    "category": {
+      "id": 1,
+      "name": "Dogs"
+    },
+    "photoUrls": [
+      "string"
+    ],
+    "tags": [
+      {
+        "id": 8,
+        "name": "friendly"
+      }
+    ],
+    "status": "available"
+}
+
+export async function makeARequest(endpointURL: string, request: APIRequest): Promise<Response>{
+  return await fetch(endpointURL, request);
+  // if(response.ok){
+  //   // if(request.method == "DELETE"){
+  //   //   return await response.text();
+  //   // }else{
+  //   //   return await response.json();
+  //   //  }
+  //   return response
+  // }else{
+  //   return ("API Request Error --> Response:"+response)
+  // }
+ 
+}
+
+export async function createANewPet(pet_info:pet_info):Promise<Response>{
+  const newPet:APIRequest ={
+      method: "POST",
+      body: JSON.stringify(pet_info),
+      headers: {"Content-Type":"application/json"}
+  }
+  return await makeARequest(baseURL+"/pet",newPet)
+  // console.log("Pet added successfully:", await data.json())
+  
+  //expect(data).toEqual(pet_info)
+//   //const petAPI = new PetRequests();
+//   try{
+//     const newPet = petAPI.addNewPet({
+//       "id": 1017249723,
+//       "name": "Cosmo",
+//       "category": {
+//         "id": 1,
+//         "name": "Dogs"
+//       },
+//       "photoUrls": [
+//         "string"
+//       ],
+//       "tags": [
+//         {
+//           "id": 8,
+//           "name": "friendly"
+//         }
+//       ],
+//       "status": "available"
+//   });
+//   const data = await petAPI.makeARequest(newPet)
+//   console.log("Pet added successfully:", data)
+// }catch(error){
+//   console.error ( "Error adding pet:", error)
+// }
 }
 
 async function getAPetById(){
@@ -78,15 +146,15 @@ async function deleteAPet(){
   }
 }
 
-async function testAPIservice(){
-  await createANewPet();
-  await getAPetById();
-  await updateAPet();
-  await getAPetById();
-  await deleteAPet();
-  await getAPetById();
-}
+// async function testAPIservice(){
+//   await createANewPet(cosmo);
+//   // await getAPetById();
+//   // await updateAPet();
+//   // await getAPetById();
+//   // await deleteAPet();
+//   // await getAPetById();
+// }
 
-testAPIservice();
+// testAPIservice();
 
 //postData();
