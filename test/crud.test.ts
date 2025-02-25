27ea039f8beb15cc6ff03bacb78b2af0f65fb916
JPKingtero <JPQM4897@gmail.com>
2025-02-25 00:09:11 -0500

    test(crud.test.ts): quick fix

--- test/crud.test.ts
+++ test/crud.test.ts
@@ -1,7 +1,51 @@
 import { createANewPet } from "../src/api"
+import { pet_info } from "../src/petRequest"
+
 
 describe('Create, Read, Update and Delete',()=> {
-    test('Create a new pet', ()=>{
-        const apiPetResquest = createANewPet()
+    const newPetValues:pet_info = {
+        "id": 1017249723,
+        "name": "Cosmo",
+        "category": {
+        "id": 1,
+        "name": "Dogs"
+        },
+        "photoUrls": [
+        "string"
+        ],
+        "tags": [
+        {
+            "id": 8,
+            "name": "friendly"
+        }
+        ],
+        "status": "available"
+    }
+
+    const updatedPetValues:pet_info = {
+        "id": 1017249723,
+        "name": "Cosmo(Updated",
+        "category": {
+        "id": 1,
+        "name": "Dogs"
+        },
+        "photoUrls": [
+        "string"
+        ],
+        "tags": [
+        {
+            "id": 8,
+            "name": "friendly"
+        }
+        ],
+        "status": "available"
+    }
+
+
+    test('Create a new pet', async ()=>{
+        const response = await createANewPet(newPetValues)
+        const json = await response.json();
+        console.log(json);
+        expect(json).toEqual(newPetValues)
     })
 })
