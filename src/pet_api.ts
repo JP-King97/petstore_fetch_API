2ae74959d0f01fa1a6f7b2de0f560d9bf2e0a3ac
JPKingtero <JPQM4897@gmail.com>
2025-02-25 23:34:38 -0500

    refactor(pet_api): improvement for code reusability

--- src/pet_api.ts
+++ src/pet_api.ts
@@ -30,36 +30,48 @@ export async function makeARequest(endpointURL: string, request: APIRequest): Pr
   return await fetch(endpointURL, request);
 }
 
-export async function createANewPet(pet_info:pet_info):Promise<Response>{
-  const newPet:APIRequest ={
-      method: "POST",
-      body: JSON.stringify(pet_info),
-      headers: {"Content-Type":"application/json"}
+export async function asambleRequest(method: "GET" | "POST" | "PUT" | "DELETE", body?: pet_info): Promise<APIRequest>{
+  return  {
+        method: method,
+        body: JSON.stringify(body),
+        headers: {"Content-Type":"application/json"}
   }
+}
+
+export async function createANewPet(pet_info:pet_info):Promise<Response>{
+  const newPet:APIRequest = await asambleRequest("POST", pet_info);
+  // const newPet:APIRequest ={
+  //     method: "POST",
+  //     body: JSON.stringify(pet_info),
+  //     headers: {"Content-Type":"application/json"}
+  // }
   return await makeARequest(baseURL+"/pet",newPet)
 }
 
 export async function getAPetById(pet_id:number):Promise<Response>{
-  const getPetById:APIRequest={
-    method: "GET",
-    headers:{"Content-Type":"application/json"}
-  }
+  const getPetById:APIRequest = await asambleRequest("GET")
+  // const getPetById:APIRequest={
+  //   method: "GET",
+  //   headers:{"Content-Type":"application/json"}
+  // }
   return await makeARequest(`${baseURL}/pet/${pet_id}`,getPetById)
 }
 
 export async function updateAPet(updated_pet_info:pet_info):Promise<Response>{
-  const updatePet:APIRequest ={
-    method: "PUT",
-    body: JSON.stringify(updated_pet_info),
-    headers: {"Content-Type":"application/json"}
-  }
+  const updatePet:APIRequest = await asambleRequest("PUT", updated_pet_info)
+  // const updatePet:APIRequest ={
+  //   method: "PUT",
+  //   body: JSON.stringify(updated_pet_info),
+  //   headers: {"Content-Type":"application/json"}
+  // }
   return await makeARequest(`${baseURL}/pet`,updatePet)
 }
 
 export async function deleteAPet(pet_id:number){
-  const deletePet:APIRequest = {
-    method:"DELETE",
-    headers:{"Content-Type":"application/json"}
-  }
+  const deletePet:APIRequest = await asambleRequest("DELETE");
+  // const deletePet:APIRequest = {
+  //   method:"DELETE",
+  //   headers:{"Content-Type":"application/json"}
+  // }
   return await makeARequest(`${baseURL}/pet/${pet_id}`,deletePet)
 }
