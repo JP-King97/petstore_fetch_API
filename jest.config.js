cbfc8bee34efe6fb46f11f3d3b491a31d99807e5
JPKingtero <JPQM4897@gmail.com>
2025-02-26 02:01:32 -0500

    ci: implementation of GitHub pages

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     preset:`ts-jest`,
     testMatch: [`**/*.test.ts`],
+    reporters: [
+        "default",
+        ["jest-html-reporter", {
+          pageTitle: "Test Report",
+          outputPath: "test-results/report.html",
+          includeFailureMsg: true,
+          includeSuiteFailure: true
+        }]
+      ],
 }
--- src/pet_api.ts
+++ src/pet_api.ts
@@ -40,38 +40,20 @@ export async function asambleRequest(method: "GET" | "POST" | "PUT" | "DELETE", 
 
 export async function createANewPet(pet_info:pet_info):Promise<Response>{
   const newPet:APIRequest = await asambleRequest("POST", pet_info);
-  // const newPet:APIRequest ={
-  //     method: "POST",
-  //     body: JSON.stringify(pet_info),
-  //     headers: {"Content-Type":"application/json"}
-  // }
   return await makeARequest(baseURL+"/pet",newPet)
 }
 
 export async function getAPetById(pet_id:number):Promise<Response>{
   const getPetById:APIRequest = await asambleRequest("GET")
-  // const getPetById:APIRequest={
-  //   method: "GET",
-  //   headers:{"Content-Type":"application/json"}
-  // }
   return await makeARequest(`${baseURL}/pet/${pet_id}`,getPetById)
 }
 
 export async function updateAPet(updated_pet_info:pet_info):Promise<Response>{
   const updatePet:APIRequest = await asambleRequest("PUT", updated_pet_info)
-  // const updatePet:APIRequest ={
-  //   method: "PUT",
-  //   body: JSON.stringify(updated_pet_info),
-  //   headers: {"Content-Type":"application/json"}
-  // }
   return await makeARequest(`${baseURL}/pet`,updatePet)
 }
 
 export async function deleteAPet(pet_id:number){
   const deletePet:APIRequest = await asambleRequest("DELETE");
-  // const deletePet:APIRequest = {
-  //   method:"DELETE",
-  //   headers:{"Content-Type":"application/json"}
-  // }
   return await makeARequest(`${baseURL}/pet/${pet_id}`,deletePet)
 }
