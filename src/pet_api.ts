0092eea4b3272b3b31a410f88fbcbaf9eb9165a2
JPKingtero <JPQM4897@gmail.com>
2025-02-27 23:17:09 -0500

    ci 8

--- src/pet_api.ts
+++ src/pet_api.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker/.";
 
 export interface APIRequest {
   method: "GET" | "POST" | "PUT" | "DELETE" ;
@@ -37,6 +38,55 @@ export async function asambleRequest(method: "GET" | "POST" | "PUT" | "DELETE", 
         headers: {"Content-Type":"application/json"}
   }
 }
+
+export function newPetValues():pet_info{
+  const photoUrls: string = faker.internet.url();
+  const pet_id:number = faker.number.int(12000);
+  const pet_category_id: number = faker.number.int(20) 
+  const pet_species: string = faker.animal.type();
+  const pet_name: string = faker.animal.petName()
+
+  return {
+      "id": pet_id,
+      "name": pet_name,
+      "category": {
+      "id": pet_category_id,
+      "name": pet_species
+      },
+      "photoUrls": [
+      photoUrls
+      ],
+      "tags": [
+      {
+          "id": faker.number.int(10),
+          "name": "friendly"
+      }
+      ],
+      "status": "available"
+  }
+}
+
+export function updatedPetValues(pet_info:pet_info):pet_info{
+  return {
+    "id": pet_info.id,
+    "name": pet_info.name+"(Updated)",
+    "category": {
+    "id": pet_info.category.id,
+    "name": pet_info.category.name
+    },
+    "photoUrls": [
+      pet_info.photoUrls[0]
+    ],
+    "tags": [
+    {
+        "id": pet_info.tags[0].id,
+        "name": pet_info.tags[0].name
+    }
+    ],
+    "status": pet_info.status
+}
+}
+
 
 export async function createANewPet(pet_info:pet_info):Promise<Response>{
   const newPet:APIRequest = await asambleRequest("POST", pet_info);
--- test/crud.test.ts
+++ test/crud.test.ts
@@ -1,77 +1,45 @@
 import exp from "constants"
-import { createANewPet, deleteAPet, getAPetById, updateAPet } from "../src/pet_api"
+import { createANewPet, deleteAPet, getAPetById, newPetValues, updateAPet, updatedPetValues } from "../src/pet_api"
 import { pet_info } from "../src/pet_api"
 import { faker } from "@faker-js/faker/."
+import { setTimeout } from "timers/promises"
 
 
-describe('Create, Read, Update and Delete',()=> {
-    const photoUrls: string = faker.internet.url();
-    const pet_id:number = faker.number.int(1000);
-    const pet_category_id: number = faker.number.int(20) 
-    const pet_species: string = faker.animal.type();
-    const pet_name: string = faker.animal.petName()
-    const newPetValues:pet_info = {
-        "id": pet_id,
-        "name": pet_name,
-        "category": {
-        "id": pet_category_id,
-        "name": pet_species
-        },
-        "photoUrls": [
-        photoUrls
-        ],
-        "tags": [
-        {
-            "id": 7,
-            "name": "friendly"
-        }
-        ],
-        "status": "available"
-    }
-
-    const updatedPetValues:pet_info = {
-        "id": pet_id,
-        "name": pet_name+"(Updated)",
-        "category": {
-        "id": pet_category_id,
-        "name": pet_species
-        },
-        "photoUrls": [
-            photoUrls
-        ],
-        "tags": [
-        {
-            "id": 7,
-            "name": "friendly"
-        }
-        ],
-        "status": "available"
-    }
-
+describe('Create, Read, Update and Delete', ()=> {
+    
     test('Create a new pet', async ()=>{
-        const response = await createANewPet(newPetValues);
+        const request = newPetValues();
+        const response = await createANewPet(request);
         const json = await response.json();
-        expect(json).toEqual(newPetValues);
+        expect(json).toEqual(request);
     })
-
-    test('Get a pet by ', async ()=>{
-        const response = await getAPetById(newPetValues.id);
-        const json = await response.json();
-        expect(json).toEqual(newPetValues);
+    test('Get a pet by', async ()=>{
+        const request = newPetValues();
+        const newPet = await createANewPet(request);
+        const res2 = await getAPetById(request.id);
+        const json2 = await res2.json();
+        expect(json2).toEqual(request);
     })
-
     test('Update a pet', async ()=> {
-        const response = await updateAPet(updatedPetValues);
+        const request = newPetValues();
+        const res1 = await createANewPet(request);
+        const json1 = await res1.json();
+        //console.log(await json1)
+        const updatedRes1 = updatedPetValues(json1)
+        const response = await updateAPet(updatedRes1);
         const json = await response.json();
-        expect(json).toEqual(updatedPetValues)
+        //console.log(await json)
+        expect(json).toEqual(updatedRes1);
     })
-
     test('Delete a pet', async ()=> {
-        const res1 = await deleteAPet(updatedPetValues.id);
-        const resText1 = await res1.text();
+        const request = newPetValues();
+        const res1 = await createANewPet(request);
+        const json = await res1.json()
+        const res2 = await deleteAPet(json.id);
+        const resText1 = await res2.text();
         expect(resText1).toEqual("Pet deleted");
-        const res2 = await getAPetById(updatedPetValues.id);
-        const resText2 = await res2.text();
+        const res3 = await getAPetById(json.id);
+        const resText2 = await res3.text();
         expect(resText2).toEqual("Pet not found")
     })
 })
